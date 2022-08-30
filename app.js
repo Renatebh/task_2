3ccb94a9593ad24fc0c3398125153ff3c84f2d46
Renatebh <renate.hem@gmail.com>
2022-08-30 12:46:17 +0200

    message

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("App.js");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+app.listen(port, () => {
+  console.log(`Server on port: ${port}`);
+});
